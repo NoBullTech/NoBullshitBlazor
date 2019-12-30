0eefdc4bd0af68877edbbb1987d6015c2bf48df1
Larry Schoeneman <lschoeneman@gmail.com>
2019-12-30 17:11:07 -0600

    Added databinding, persondisplay, etc.

---
+++ Shared/PersonDisplay.Razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using NoBullshitBlazor.Data;
+
+namespace NoBullshitBlazor.Shared
+{
+    public partial class PersonDisplay
+    {
+        [Parameter] 
+        public Person Person { get; set; }
+    }
+}
